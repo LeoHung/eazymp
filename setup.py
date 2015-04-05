48398a861d6c0e1ee75e792a00dbc67dead6358f
Leo Hung <c2016.tw@gmail.com>
2015-04-05 17:18:26 -0400

    fix includes

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 setup(
-    name = "PyMP",
-    version = "0.1",
+    name = "lazymp",
+    version = "0.1.0.0.1",
     packages = find_packages(),
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ["Pillow>=2.8.1", "pathos>=0.2a1.dev0", "pyre>=0.8.2.0-pathos", "processing>=0.52-pathos"],
+    install_requires = [
+        "pyre>=0.8.2.0-pathos", 
+        "processing>=0.52-pathos", 
+        "ppft>=1.6.4.5",
+        "dill>=0.2.2",
+        "Pillow>=2.8.1", 
+        "pathos>=0.2a1.dev0", 
+        "ppft>=1.6.4.5",
+        "dill>=0.2.2",
+        "pyre>=0.8.2.0-pathos", 
+        "processing>=0.52-pathos"    
+    ],
 
     dependency_links = [
         "https://github.com/uqfoundation/pathos/raw/master/external/pyre-0.8.2.0-pathos.zip",
@@ -19,6 +30,6 @@ setup(
     description = "This is an Example Package",
     license = "MIT",
     keywords = "easy lazy openmp parallel",
-    url = "http://leohung.net/pymp/",   # project home page, if any
+    url = "http://leohung.net/pymp",   # project home page, if any
     # could also include long_description, download_url, classifiers, etc.
 )
