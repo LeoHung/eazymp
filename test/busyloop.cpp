637af54be9e00e131c220af60cb2a4c35e1ccdb4
leoh <c2016.tw@gmail.com>
2015-04-17 20:55:35 -0400

    renmae busyloop.c->cpp

--- test/busyloop.cpp
+++ test/busyloop.cpp
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-    int l_i = 0;
+    //int l_i = 0;
     int l = 0;
-    int i = 0;
+    //int i = 0;
 
     if(argc < 2){
         printf("Error: please specify the work size.\n");
@@ -13,9 +13,9 @@ int main(int argc, char **argv){
 
     l = atoi(argv[1]);
 
-    #pragma omp parallel for private(l_i) private(i)
-    for(l_i = 0; l_i < l; l_i++){
-        i = 0;
+    #pragma omp parallel for
+    for(int l_i = 0; l_i < l; l_i++){
+        int i = 0;
         while(i < 5000 * 1000){
             i+=1;
         }
