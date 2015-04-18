8cbc9d1be5577d311a0cef75d6ab49168559d763
leoh <c2016.tw@gmail.com>
2015-04-17 21:28:41 -0400

    fix blank lines

--- test/test_mandelbrot-thread.py
+++ test/test_mandelbrot-thread.py
@@ -2,12 +2,11 @@ from experiment import Desc, Experiment
 
 
 if __name__ == "__main__":
-    # for size in [50, 100, 200, 500]:
-    print "Mandelbort-py-thread: number of work x speedup"
+    print "Mandelbort-py-thread: work size x speedup"
     for size in [50, 100, 200, 500, 700, 1000]:
         print "Size: %d" % size
         exp = Experiment(Desc("python mandelbrot-threads.py %d %d" % (1, size), "base"),
                 [
-                    Desc("python mandelbrot-threads.py %d %d" % (p, size), "%d" %(p)) for p in [2, 4, 6, 8, 12, 16, 18, 32, 64]
+                    Desc("python mandelbrot-threads.py %d %d" % (p, size), "%d" %(p)) for p in [2, 4, 6, 8, 12, 16, 32, 64]
                 ])
         exp.table()
--- test/test_mandelbrot.py
+++ test/test_mandelbrot.py
@@ -2,11 +2,11 @@ from experiment import Desc, Experiment
 
 
 if __name__ == "__main__":
-    # for size in [50, 100, 200, 500]:
-    for size in [50, 100]:
+    print "Mandelbort: work size x speedup"
+    for size in [50, 100, 200, 500, 700, 1000]:
         print "Size: %d" % size
         exp = Experiment(Desc("python sample-mandelbrot.py %d" % size, "base"),
                 [
-                    Desc("lazymp -p%d sample-mandelbrot.py %d" % (p, size), "%d" %(p)) for p in [2, 4, 6, 8, 12, 16, 18, 32, 64]
+                    Desc("lazymp -p%d sample-mandelbrot.py %d" % (p, size), "%d" %(p)) for p in [2, 4, 6, 8, 12, 16, 32, 64]
                 ])
         exp.table()
