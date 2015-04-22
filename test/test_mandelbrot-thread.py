49c575e8e83f01a69a290ed74dc8801c91fcfdc7
leoh <c2016.tw@gmail.com>
2015-04-21 22:19:54 -0400

    change to compare single version code

--- test/test_mandelbrot-thread.py
+++ test/test_mandelbrot-thread.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     print "Mandelbort-py-thread: work size x speedup"
     for size in [50, 100, 200, 500, 700, 1000]:
         print "Size: %d" % size
-        exp = Experiment(Desc("python mandelbrot-threads.py %d %d" % (1, size), "base"),
+#        exp = Experiment(Desc("python mandelbrot-threads.py %d %d" % (1, size), "base"),
+        exp = Experiment(Desc("python sample-mandelbrot.py %d" % (size), "base"),
                 [
                     Desc("python mandelbrot-threads.py %d %d" % (p, size), "%d" %(p)) for p in [2, 4, 6, 8, 12, 16, 32, 64]
                 ])
