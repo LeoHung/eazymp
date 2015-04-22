7240b011d5b8c39e4d26438cb4f9afeef3d86689
leoh <c2016.tw@gmail.com>
2015-04-21 22:18:09 -0400

    change to compare single version code

--- test/test_busyloop-threads.py
+++ test/test_busyloop-threads.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     print "Busyloop-py-thread: work size x speedup"
     for size in [50, 100, 200, 300, 400, 500]:
         print "Size: %d" % size
-        exp = Experiment(Desc("python busyloop-threads.py %d %d" % (1, size), "base"),
+#        exp = Experiment(Desc("python busyloop-threads.py %d %d" % (1, size), "base"),
+        exp = Experiment(Desc("python busyloop.py %d" % (size), "base"),
                 [
                     Desc("python busyloop-threads.py %d %d" % (p, size), "%d" %(p)) for p in [2, 4, 6, 8, 12, 16, 32, 64]
                 ])
