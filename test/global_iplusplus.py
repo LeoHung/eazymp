8f1d5185514e45d526fa2ada1b7d04ea5175738b
sanchuah <sanchuah@andrew.cmu.edu>
2015-05-09 22:03:35 -0400

    add global_iplusplus.py to test shared number

---
+++ test/global_iplusplus.py
@@ -0,0 +1,11 @@
+from lazymp.atomic import Atomic
+def iplusplus(num_round):
+    global_i = 0 #pragma shared number
+    for _ in range(num_round): #pragma omp parallel for
+        for __ in range(5):
+            with Atomic():
+                global_i += 1
+    return global_i
+
+if __name__ == "__main__":
+    print iplusplus(10)
